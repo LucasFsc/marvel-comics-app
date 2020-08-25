0d72708d107408658d41638108e2649f02e9b07f
LucasFsc <lucas.fscr@gmail.com>
2020-08-25 19:11:28 -0300

    feat(app): improve prettier and add editorconfig

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
 
 export default function App() {
   return (
@@ -8,14 +17,5 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
