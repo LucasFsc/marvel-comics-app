f4acabc4dd4b24b047d809e0eeed968b6de771c8
LucasFsc <lucas.fscr@gmail.com>
2020-08-25 20:50:19 -0300

    feat(app): add ui framework

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
-    es6: true,
+    es6: true
   },
   extends: ["airbnb", "prettier", "prettier/react"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
-    __DEV__: "readonly",
+    __DEV__: "readonly"
   },
   parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["react", "prettier"],
   rules: {
@@ -22,15 +22,16 @@ module.exports = {
     "react/jsx-filename-extension": [
       "warn",
       {
-        extensions: [".jsx", ".js"],
-      },
+        extensions: [".jsx", ".js"]
+      }
     ],
     "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
     "react/state-in-constructor": "off",
     "react/static-property-placement": "off",
     "react/jsx-props-no-spreading": "off",
     "react/prop-types": "off",
     "no-param-reassign": "off",
-    "no-console": "off",
-  },
-};
+    "no-console": "off"
+  }
+}
---
+++ metro.config.js
@@ -0,0 +1,7 @@
+const MetroConfig = require("@ui-kitten/metro-config")
+
+const evaConfig = {
+  evaPackage: "@eva-design/eva"
+}
+
+module.exports = MetroConfig.create(evaConfig, {})
