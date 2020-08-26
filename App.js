e67189909b3ed81bbf1f854fc56c1ebac2434d00
Lucas Ferraz Schlemper <lucas.fscr@gmail.com>
2020-08-25 22:33:59 -0300

    feat(navigation): add navigation structure (#4)
    * feat(app): add navigation dependencies
    
    * feat(navigation): add app navigator and main screen dummy component

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from "react"
 import * as eva from "@eva-design/eva"
 import { ApplicationProvider } from "@ui-kitten/components"
 import theme from "./theme.json"
+import Navigator from "~/router"
 
 export default () => (
-  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} />
+  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+    <Navigator />
+  </ApplicationProvider>
 )
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+
+import { Main } from "~/screens"
+
+const Stack = createStackNavigator()
+
+export default () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Main">
+      <Stack.Screen name="Main" component={Main} />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
