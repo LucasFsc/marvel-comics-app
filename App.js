ac6b0a4ad038739b9e2364e5a4ce9c8496a33638
Lucas Ferraz Schlemper <lucas.fscr@gmail.com>
2020-08-25 21:17:42 -0300

    feat(app): add ui framework (#2)

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider } from "@ui-kitten/components"
+import theme from "./theme.json"
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-})
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+export default () => (
+  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} />
+)
